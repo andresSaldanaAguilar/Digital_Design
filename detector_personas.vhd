af3b842c8359923738fbdf0f98f1d9b0cfcab853
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2017-06-09 21:00:35 -0500

    Contador funcionando, falta probar displays

--- detector_personas.vhd
+++ detector_personas.vhd
@@ -6,7 +6,8 @@ use ieee.std_logic_unsigned.all;
 entity dp is port(
 	clk: in std_logic;
 	X:in std_logic_vector(1 downto 0);
-	-- unidades
+	ctrl: inout std_logic_vector(1 downto 0);
+	--unidades
 	unidades: out STD_LOGIC_VECTOR(3 downto 0);
 	-- decenas
 	decenas: out STD_LOGIC_VECTOR(3 downto 0);
@@ -17,40 +18,43 @@ entity dp is port(
 	-- salidas a transistores
 	trans1,trans2: inout std_logic;
 	salida: inout std_logic_vector(7 downto 0));
-	
 
 	attribute loc: string;
 	
 	attribute loc of clk: signal is "p4";
 	attribute loc of X: signal is "p96,p95";
 
-	--attribute loc of displays: signal is "p125,p124,p123,p122,p121,p98,p97";
+	--attribute loc of display: signal is "p125,p124,p123,p122,p121,p98,p97";
 	attribute loc of salida: signal is "p88,p87,p86,p85,p84,p83,p81,p80";	
 end;
 
 architecture a_dp of dp is
-	--Constantes para los displays
-	constant DIG0: std_logic_vector (6 downto 0):="0000001";--0
-	constant DIG1: std_logic_vector (6 downto 0):="1001111";--1
-	constant DIG2: std_logic_vector (6 downto 0):="0010010";--2
-	constant DIG3: std_logic_vector (6 downto 0):="0000110";--3
-	constant DIG4: std_logic_vector (6 downto 0):="1001100";--4
-	constant DIG5: std_logic_vector (6 downto 0):="0100100";--5
-	constant DIG6: std_logic_vector (6 downto 0):="0100000";--6
-	constant DIG7: std_logic_vector (6 downto 0):="0001111";--7
-	constant DIG8: std_logic_vector (6 downto 0):="0000000";--8
-	constant DIG9: std_logic_vector (6 downto 0):="0000100";--9
-	constant DIGERROR: std_logic_vector (6 downto 0):="0110000";
+	
+		--Constantes para los displays
+		constant DIG0: std_logic_vector (6 downto 0):="0000001";--0
+		constant DIG1: std_logic_vector (6 downto 0):="1001111";--1
+		constant DIG2: std_logic_vector (6 downto 0):="0010010";--2
+		constant DIG3: std_logic_vector (6 downto 0):="0000110";--3
+		constant DIG4: std_logic_vector (6 downto 0):="1001100";--4
+		constant DIG5: std_logic_vector (6 downto 0):="0100100";--5
+		constant DIG6: std_logic_vector (6 downto 0):="0100000";--6
+		constant DIG7: std_logic_vector (6 downto 0):="0001111";--7
+		constant DIG8: std_logic_vector (6 downto 0):="0000000";--8
+		constant DIG9: std_logic_vector (6 downto 0):="0000100";--9
+		constant DIGERROR: std_logic_vector (6 downto 0):="0110000";
+		type estados is(A,B,C,D,E);
+		signal edo_presente,edo_futuro:estados; 	
+		begin
 
-	type estados is(A,B,C,D,E);
-	signal edo_presente,edo_futuro:estados;	
-	begin
-		process(edo_presente,X)
+
+
+		process(edo_presente,X,salida)
 			begin
+
 			case edo_presente is
 			
 			-------------------------------------caso base
-			when A=>salida<=salida;	
+			when A=>ctrl<="00";	
 			if(X="10")then
 			edo_futuro<=D;
 			elsif(X="01")then --es necesario especificar el estado de X y Y?
@@ -60,60 +64,74 @@ architecture a_dp of dp is
 			end if;
 
 			-------------------------------------alguien entra
-			when B=>	
+			when B=>ctrl<="00";
 			if(X="10")then
 			edo_futuro<=C;
 			else 
-			salida<=salida;
 			edo_futuro<=B;
 			end if;
 			
-			when C=>if(X="11")then
-			salida<=salida+"00000001"; --XD	
+			when C=>
+			if(X="11")then
+			--salida<=salida+"00000001; --XD	
+			ctrl<="10";
 			edo_futuro<=A;
 			else 
-			salida<=salida;	
+			ctrl<="00";
 			edo_futuro<=C;
 			end if;
 
 			-------------------------------------alguien sale
-			when D=>salida<=salida;
+			when D=>ctrl<="00";
 			if(X="01")then
 			edo_futuro<=E;
 			else --mando al edo inicial?
 			edo_futuro<=D;
 			end if;
 
-			when E=>if(X="11")then
-			salida<=salida-"00000001"; --XD	
+			when E=>
+			if(X="11")then
+			ctrl<="01";
 			edo_futuro<=A;
 			else 
-			salida<=salida;	
+			ctrl<="00";	
 			edo_futuro<=E;
 			end if;
 			end case;
+			
 	
 		end process;
-	
-		process(clk,edo_presente,edo_futuro)
+		process(ctrl,salida)
 			begin
 
 			if(clk'event and clk='1') then
-				edo_presente<=edo_futuro;			
+					case ctrl is
+					when"00"=>salida<=salida;
+					when"10"=>salida<=salida+"00000001";
+					when"01"=>salida<=salida-"00000001";
+					when others=>salida<=salida;
+				end case;		
 			end if;
 		end process;
-------------------------------------
+	
+		process(clk,edo_presente,edo_futuro,salida)
+			begin
 
-		--Proceso que convierte de binario a bcd
+			if(clk'event and clk='1') then
+				edo_presente<=edo_futuro;		
+			end if;
+		end process;
+
+	--Proceso que convierte de binario a bcd
 		convertidor: process(salida)
-		--Variable auxiliar que nos sirve para hacer corrimientos mÃ¡s fÃ¡ciles
+		--Variable auxiliar que nos sirve para hacer corrimientos más fáciles
 		variable aux: STD_LOGIC_VECTOR(15 downto 0);
 		begin
 			--Inicializamos todos los datos en cero.
 			aux := (others => '0');
 			--Cargamos los datos por primera vez.
 			aux(7 downto 0) := salida;
-			--Por cada bit habrÃ¡ que hacer un corrimiento a la izquierda
+			--Por cada bit habrá que hacer un corrimiento a la izquierda
 			--Eso determina las repeticiones del for, en este caso 7
 			for i in 0 to 7 loop
 				--Unidades (4 bits).
@@ -126,7 +144,7 @@ architecture a_dp of dp is
 				if aux(15 downto 12) > 4 then
 					aux(15 downto 12) := aux(15 downto 12) + 3;
 				end if;
-				--Corrimiento a la izquierda al estilo AndrÃ©s, podriamos intentar con un ROL o SLR haber que pasa
+				--Corrimiento a la izquierda al estilo Andrés, podriamos intentar con un ROL o SLR haber que pasa
 				aux(15 downto 1) := aux(14 downto 0);
 		end loop;
 		--Pasando datos de variable aux a la correspondiente salida.
@@ -179,5 +197,6 @@ architecture a_dp of dp is
 			when others =>displayd<=DIGERROR;
 		end case ;
 	end process pren_dis;
-end a_dp;
+
+	end a_dp;
 
